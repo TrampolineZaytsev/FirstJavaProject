8a03db94cbe19ba25e5cfa44a64d0e0788ec3d1c
TrampolineZaytsev <Zaytsev_PavelDm@mail.ru>
2022-10-29 15:59:40 +0300

    Init 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!"); //comit
+
+        int i = 5;
+        float f = 4.2f;
+        int b = (int)f / 2;
+
+        String str = "222";
+        int strInt = Integer.parseInt(str);
+        System.out.println(strInt);
+
+
         m();
     }
     public static void m()
     {
-        System.out.println("Hello!"); //comit
+        System.out.println("End!"); //comit
     }
 }
